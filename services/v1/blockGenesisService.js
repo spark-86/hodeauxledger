d1030947d06f306c46ae68066391633c4783a1be
Veronica Hodo <veronica@veronicahodo.com>
2025-07-03 11:40:56 -0700

    Fix import statement for keyCleaner.js in blockGenesisService.js

--- services/v1/blockGenesisService.js
+++ services/v1/blockGenesisService.js
@@ -1,7 +1,7 @@
 import { Record } from "../../services/v1/recordService.js";
 import { Keyring } from "../../services/v1/keyringService.js";
 import fs from "fs";
-import { keyClean } from "../../tools/keyCleaner";
+import { keyClean } from "../../tools/keyCleaner.js";
 
 const ledgerPath = process.env.LEDGER_PATH || "/ledger";
 const keyPath = process.env.KEY_PATH || "/secrets";
