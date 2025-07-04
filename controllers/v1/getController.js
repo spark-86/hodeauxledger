5efd823ed02bc45c3e2265b5979d13c0797b2a7a
Veronica Hodo <veronica@veronicahodo.com>
2025-07-04 09:02:44 -0700

    Refactor getPreviousHash to handle undefined scope and improve file path consistency

--- controllers/v1/getController.js
+++ controllers/v1/getController.js
@@ -6,11 +6,12 @@ const ledgerPath = process.env.LEDGER_PATH || "/ledger";
 export const getPreviousHash = async (req, res) => {
     const { scope } = req.params;
 
-    if (!fs.existsSync(`${ledgerPath}/lastHash_${scope}.txt`))
+    const workingScope = scope ? scope : "";
+    if (!fs.existsSync(`${ledgerPath}/lastHash_${workingScope}.txt`))
         return res.status(404).json({ error: "No previous hash found" });
 
     const lastHash = fs.readFileSync(
-        `${ledgerPath}/lastHash_${scope}.txt`,
+        `${ledgerPath}/lastHash_${workingScope}.txt`,
         "utf8"
     );
     return res.status(200).json({ data: lastHash });
