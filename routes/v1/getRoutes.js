88357ee5018b491ba34730293ce3c6e67ea9715f
Veronica Hodo <veronica@veronicahodo.com>
2025-07-04 09:01:07 -0700

    Fix duplicate route definition for getPreviousHash endpoint

--- routes/v1/getRoutes.js
+++ routes/v1/getRoutes.js
@@ -4,5 +4,6 @@ import { getPreviousHash } from "../../controllers/v1/getController.js";
 const router = express.Router();
 
 router.get("/:scope", getPreviousHash);
+router.get("/", getPreviousHash);
 
 export default router;
